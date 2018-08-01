a0b3ab9f1a75ca4dc9578026a337815a372f0999
kavibaratir <32194416+kavibaratir@users.noreply.github.com>
2018-08-01 13:40:58 +0530

    Update StringRev.java

--- StringRev.java
+++ StringRev.java
@@ -1,15 +1,13 @@
 package hunter;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class StringRev {
 
 	public static void main(String[] args) {
-		//Scanner s=new Scanner (System.in);
-		String a="Hello  World";//s.nextLine();
-		
+		Scanner s=new Scanner (System.in);
+		String a=s.nextLine();
 		String ss[]=a.split(" ");
-		int x=0;
 		for(int i=0;i<ss.length;i++) {
 			char ch[]=ss[i].toCharArray();
 			for (int j = ch.length-1; j >=0; j--) {
